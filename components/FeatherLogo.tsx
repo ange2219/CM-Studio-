4885d8ca28a9760ba83e607e73548cf7f1460f66
unknown <angedahou7@gmail.com>
2026-07-26 15:16:17 +0200

    fix(logo): add cache-busting v=2 to force load the correct transparent blue logo

--- components/FeatherLogo.tsx
+++ components/FeatherLogo.tsx
@@ -6,7 +6,7 @@ export function FeatherLogo({ className = "w-6 h-6", darkMode = false }: { class
   return (
     <div className="relative flex flex-col items-center justify-center shrink-0">
       <img
-        src="/logo-blue.png"
+        src="/logo-blue.png?v=2"
         alt="Logo CM Studio"
         className={className}
         style={{ objectFit: 'contain' }}
